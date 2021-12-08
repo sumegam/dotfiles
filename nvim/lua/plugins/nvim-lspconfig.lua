05d1cca41d0524aef03c6eb289a3baaf0029cc7a
Miroslav Sumega <miro.sumega@gmail.com>
2021-12-08 19:24:22 +0100

    Add eslint language server

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -49,7 +49,7 @@ local nvim_lsp = require('lspconfig')
 -- Use a loop to conveniently call 'setup' on multiple servers with default
 -- configuration and map buffer local keybindings when the language server
 -- attaches
-local servers = { 'pyright', 'gopls', 'pylsp', 'html', 'cssls', 'tsserver' }
+local servers = { 'pyright', 'gopls', 'pylsp', 'html', 'cssls', 'tsserver', 'eslint' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
